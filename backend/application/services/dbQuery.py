bcbbbf8f281c3240a0719f2ab1517f9fdac18f93
StevenGong333 <120246964+Roszillary@users.noreply.github.com>
2023-05-24 14:15:28 -0700

    Update dbQuery.py

--- backend/application/services/dbQuery.py
+++ backend/application/services/dbQuery.py
@@ -7,3 +7,27 @@ class DatabaseQuery:
         args = [users.get_fname(), users.get_lname(), users.get_email(), users.get_username(), users.get_password_hash()]
         db.execute(query, args)
         db.commit()
+
+    def retrieveUserIdSession(self, db, users):
+        query = "SELECT `ID` FROM `users` WHERE `username` = %s AND `password` = %s LIMIT 1"
+        args = [users.get_username(), users.get_password_hash()]
+        ret = db.fetchone(query, args)
+        return ret[0]
+
+    def createSession(self, db, sessions):
+        query = "INSERT INTO `sessions`(`fk_user_ID`, `token`) VALUES (%s, %s)"
+        args = [sessions.get_fk_user_ID(), sessions.get_token()]
+        db.execute(query, args)
+        db.commit()
+
+    def retrieveUserIdToken(self, db, sessions):
+        query = "SELECT `fk_user_ID` FROM `sessions` WHERE `token` = %s LIMIT 1"
+        args = [sessions.get_token()]
+        ret = db.fetchone(query, args)
+        return ret[0]
+
+    def createMessage(self, db, messages):
+        query = "INSERT INTO `messages`(`fk_user_ID`, `message`) VALUES (%s, %s)"
+        args = [messages.get_fk_user_ID(), messages.get_message()]
+        db.execute(query, args)
+        db.commit()
